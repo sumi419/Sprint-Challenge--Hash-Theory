c8e023fbb0062d2ab65a349c2a287a02d479b696
sumi419 <sasgar04@gmail.com>
2019-03-01 09:29:55 -0800

    ex1.c done ⚖️✨

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -8,15 +8,36 @@ Answer *get_indices_of_item_weights(int *weights, int length, int limit)
   HashTable *ht = create_hash_table(16);
 
   // YOUR CODE HERE
-
+  int arr[length];
+  for (int i = 0; i < length; i++)
+  {
+    arr[i] = limit - weights[i];
+    hash_table_insert(ht, weights[i], i);
+  }
+  for (int i = 0; i < length; i++)
+  {
+    int index = hash_table_retrieve(ht, arr[i]);
+    if (index != -1 && index != i)
+    {
+      destroy_hash_table(ht);
+      Answer *answer = malloc(sizeof(Answer));
+      answer->index_1 = index;
+      answer->index_2 = i;
+      return answer;
+    }
+  }
+  destroy_hash_table(ht);
   return NULL;
 }
 
 void print_answer(Answer *answer)
 {
-  if (answer != NULL) {
+  if (answer != NULL)
+  {
     printf("%d %d\n", answer->index_1, answer->index_2);
-  } else {
+  }
+  else
+  {
     printf("NULL\n");
   }
 }
@@ -28,22 +49,22 @@ int main(void)
   // TEST 1
   int weights_1 = {9};
   Answer *answer_1 = get_indices_of_item_weights(&weights_1, 1, 9);
-  print_answer(answer_1);  // NULL
+  print_answer(answer_1); // NULL
 
   // TEST 2
   int weights_2[] = {4, 4};
-  Answer* answer_2 = get_indices_of_item_weights(weights_2, 2, 8);
-  print_answer(answer_2);  // {1, 0}
+  Answer *answer_2 = get_indices_of_item_weights(weights_2, 2, 8);
+  print_answer(answer_2); // {1, 0}
 
   // TEST 3
   int weights_3[] = {4, 6, 10, 15, 16};
-  Answer* answer_3 = get_indices_of_item_weights(weights_3, 5, 21);
-  print_answer(answer_3);  // {3, 1}
+  Answer *answer_3 = get_indices_of_item_weights(weights_3, 5, 21);
+  print_answer(answer_3); // {3, 1}
 
   // TEST 4
   int weights_4[] = {12, 6, 7, 14, 19, 3, 0, 25, 40};
-  Answer* answer_4 = get_indices_of_item_weights(weights_4, 9, 7);
-  print_answer(answer_4);  // {6, 2}
+  Answer *answer_4 = get_indices_of_item_weights(weights_4, 9, 7);
+  print_answer(answer_4); // {6, 2}
 
   return 0;
 }
